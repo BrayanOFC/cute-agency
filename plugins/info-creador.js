dce9d4155de58c5a900f26b9571f2fe7ef3cfe35
BrayanOFC <lourdesagueda93@gmail.com>
2025-03-29 18:52:56 -0700

    Crear info-creador.js

---
+++ plugins/info-creador.js
@@ -0,0 +1,38 @@
+// código creado por BrayanOFC
+// https://github.com/BrayanOFC
+// no quites créditos 
+
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) { 
+    let numcreador = '526633900512'; 
+    let ownerJid = numcreador + '@s.whatsapp.net';
+
+
+    let name = await conn.getName(ownerJid) || 'Brayan'; 
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
+
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+NOTE:${about}
+END:VCARD`.trim();
+
+
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: name, 
+            contacts: [{ vcard }]
+        } 
+    }, { quoted: m });
+}
+
+handler.help = ['owner']; 
+handler.tags = ['main']; 
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
